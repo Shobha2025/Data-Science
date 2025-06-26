6f0c5b37256e9b893754690ecd982ed488262e83
Shobha2025 <shobha.poojari2025@gmail.com>
2025-06-26 17:21:10 +0530

    test

--- Data_Science_intro_2.py
+++ Data_Science_intro_2.py
@@ -7,3 +7,21 @@
 # - tuple: Ordered, unchangeable collection like (1, 2) – used for fixed data.
 # - dict: Key-value pairs like {"name": "John"} – used for structured data.
 # - set: Unordered collection of unique items like {1, 2, 3} – used to remove duplicates.
+
+# 1. Variables
+name = "Shree"
+age = 25
+height = 5.4
+
+# 2. Print and Type
+print(name)          # Output: Shree
+print(type(age))     # Output: <class 'int'>
+
+# 3. Lists
+fruits = ["apple", "banana", "mango"]
+print(fruits[1])     # banana
+
+# 4. Dictionaries
+person = {"name": "Suhani", "age": 25}
+print(person["name"])    # Suhani
+print(person["age"])
