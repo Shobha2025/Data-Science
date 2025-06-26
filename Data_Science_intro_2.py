1e72dfb71813b43e296607f87f1088d8e833d7c4
Shobha2025 <shobha.poojari2025@gmail.com>
2025-06-26 17:23:55 +0530

    test

--- Data_Science_intro_2.py
+++ Data_Science_intro_2.py
@@ -1,12 +1,12 @@
-# 9. Data Types in Python
-# - int: Integer numbers like 5 or -10 – used for counting and indexing.
-# - float: Decimal numbers like 3.14 – used for precise calculations.
-# - str: Text data like "Hello" – used for labels and names.
-# - bool: Boolean values True or False – used in conditions and filters.
-# - list: Ordered, changeable collection like [1, 2, 3] – used to store multiple items.
-# - tuple: Ordered, unchangeable collection like (1, 2) – used for fixed data.
-# - dict: Key-value pairs like {"name": "John"} – used for structured data.
-# - set: Unordered collection of unique items like {1, 2, 3} – used to remove duplicates.
+"""9. Data Types in Python
+- int: Integer numbers like 5 or -10 – used for counting and indexing.
+- float: Decimal numbers like 3.14 – used for precise calculations.
+- str: Text data like "Hello" – used for labels and names.
+- bool: Boolean values True or False – used in conditions and filters.
+- list: Ordered, changeable collection like [1, 2, 3] – used to store multiple items.
+- tuple: Ordered, unchangeable collection like (1, 2) – used for fixed data.
+- dict: Key-value pairs like {"name": "John"} – used for structured data.
+- set: Unordered collection of unique items like {1, 2, 3} – used to remove duplicates."""
 
 # 1. Variables
 name = "Shree"
