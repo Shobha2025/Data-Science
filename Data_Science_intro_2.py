839695825fd49590b3c3e51dbd42273662426531
Shobha2025 <shobha.poojari2025@gmail.com>
2025-06-26 17:04:54 +0530

    Added Day 2 intro

---
+++ Data_Science_intro_2.py
@@ -0,0 +1,8 @@
+9. Data Types in Python
+- int (Integer): 5 → Used for whole numbers
+- float (Decimal): 3.14 → Used for numbers with decimal points
+- str (String): "Hello" → Used for text or characters
+- bool (Boolean): True, False → Used for logical values
+- list: [1, 2, 3] → Used for a collection of items
+- dict (Dictionary): {"name": "John"} → Used for key-value pairs
+
